123b6483e1a770468053bad1dba1217f0504cccc
Michael Yu <myu117@gmail.com>
2021-01-14 21:08:18 -0800

    Initial commit

---
+++ BaseConvert/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaseConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
